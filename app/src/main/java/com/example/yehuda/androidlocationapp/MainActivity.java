6d8de3e7744e15dc8db65c6395a1825da3b990c1
livnoni <yehuda.livnoni@gmail.com>
2016-11-28 10:08:51 +0200

    Adding button that shows the location

--- app/src/main/java/com/example/yehuda/androidlocationapp/MainActivity.java
+++ app/src/main/java/com/example/yehuda/androidlocationapp/MainActivity.java
@@ -1,52 +1,113 @@
 package com.example.yehuda.androidlocationapp;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button GpsButton;
+    private TextView CoordinationView;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    private static final int minTime = 5000; //min time in milliseconds to show new gps single
+    private static final int minDistance = 0; //min distance (in meters), to show new gps single
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        GpsButton = (Button) findViewById(R.id.button_show_gps_location);
+        CoordinationView = (TextView) findViewById(R.id.GpsCoordination);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener()
+        {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onLocationChanged(Location location)
+            {
+                CoordinationView.setText("");
+                
+                CoordinationView.append("Latitude: "+location.getLatitude()+"\n"
+                        +"Longitude"+location.getLongitude()+"\n"
+                        +"getAltitude"+location.getAltitude()+"\n"
+                        +"getSpeed"+location.getSpeed()+"\n"
+                        +"getAccuracy"+location.getAccuracy()+"\n"
+
+
+                        +"\n");
             }
-        });
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s)
+            {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s)
+            {
+                //GO to gps SETTING:
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+
+        configure_button();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        @Override
+        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+            switch (requestCode){
+                case 10:
+                    configure_button();
+                    break;
+                default:
+                    break;
+            }
         }
 
-        return super.onOptionsItemSelected(item);
-    }
+        void configure_button()
+        {
+            // first check for permissions
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET},10);
+                }
+                return;
+            }
+            // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
+            GpsButton.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view) {
+                    //noinspection MissingPermission
+
+                    locationManager.requestLocationUpdates("gps", minTime, minDistance, locationListener);
+                }
+            });
+        }
+
+
 }
+
+
