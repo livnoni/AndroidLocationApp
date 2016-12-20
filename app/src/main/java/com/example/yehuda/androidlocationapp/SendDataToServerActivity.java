b5fbf445004b2c6bd2811cc931b257a865987d0f
livnoni <yehuda.livnoni@gmail.com>
2016-12-20 02:46:39 +0200

    Added automaticlly upload location to server addidng hte ability to stop and re send. created Thread + Handler for running bagcround.

--- app/src/main/java/com/example/yehuda/androidlocationapp/SendDataToServerActivity.java
+++ app/src/main/java/com/example/yehuda/androidlocationapp/SendDataToServerActivity.java
@@ -1,16 +1,15 @@
 package com.example.yehuda.androidlocationapp;
 
-import android.*;
-import android.app.DownloadManager;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.net.http.RequestQueue;
 import android.os.Build;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -20,9 +19,9 @@ import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 import com.android.volley.AuthFailureError;
@@ -33,36 +32,28 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.text.DateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
 
 public class SendDataToServerActivity extends AppCompatActivity {
     double Altitude,Latitude,Longitude;
     String model;
     TextView altitude,latitude,longitude,result;
     Button buttonSendData,ButtonGetData;
+    Switch toggleButtonAutomaticallySendData;
 
     com.android.volley.RequestQueue requestQueue;
     private LocationManager locationManager;
     private LocationListener locationListener;
+
+    private Thread thread;
 
     //for my local phpmyadmin:
     // String insertUrl = "http://10.0.0.2/tutorial/insertStudent.php";
@@ -73,6 +64,41 @@ public class SendDataToServerActivity extends AppCompatActivity {
 
     private static final int minTime = 1000; //min time in milliseconds to show new gps single
     private static final int minDistance = 0; //min distance (in meters), to show new gps single
+
+     Handler handler = new Handler()
+    {
+        @Override
+        public void handleMessage(Message msg)
+        {
+            Log.d("sendToServer() ", "-------------------");
+            sendToServer();
+        }
+    };
+    boolean send=false;
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run()
+        {
+            //while(true)
+            {
+                synchronized (this)
+                {
+                    while(send)
+                    {
+                        try {
+                            wait(5000);
+                            handler.sendEmptyMessage(0);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }
+            }
+        }
+    };
+    Thread t = new Thread(runnable);
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -88,15 +114,20 @@ public class SendDataToServerActivity extends AppCompatActivity {
 
         buttonSendData = (Button) findViewById(R.id.button_send_data);
         ButtonGetData = (Button) findViewById(R.id.button_data_from_server);
+        toggleButtonAutomaticallySendData = (Switch) findViewById(R.id.button_automatically_send_data);
+        toggleButtonAutomaticallySendData.setBackgroundColor(Color.GREEN);
+
         model = Build.MODEL;
         requestQueue = Volley.newRequestQueue(getApplicationContext());
-
 
         turnOnGps();
         onClickButtonSendDataToServer();
         onClickButtonShowDataFromServer();
+        onClickButtonAutomaticallySendData();
 
     }
+
+
 
 
     public void turnOnGps()
@@ -165,41 +196,46 @@ public class SendDataToServerActivity extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
-                StringRequest request = new StringRequest(Request.Method.POST, insertUrl,
-                        new Response.Listener<String>()
-                        {
-                            @Override
-                            public void onResponse(String response)
-                            {
-
-                            }
-                        }, new Response.ErrorListener()
-                {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error)
-                    {
-
-                    }
-                }){
-                    @Override
-                    protected Map<String, String> getParams() throws AuthFailureError
-                    {
-                        Map<String, String> parameters = new HashMap<String, String>();
-                        parameters.put("latitude", String.valueOf(Latitude));
-                        parameters.put("longitude", String.valueOf(Longitude));
-                        parameters.put("altitude", String.valueOf(Altitude));
-                        parameters.put("model", String.valueOf(model));
-                        parameters.put("date", String.valueOf(DateFormat.getDateTimeInstance().format(new Date())));
-
-                        return parameters;
-                    }
-                };
-                Log.i("the model is", model);
-
-                requestQueue.add(request);
+                sendToServer();
             }
         });
+    }
+
+    public void sendToServer()
+    {
+        StringRequest request = new StringRequest(Request.Method.POST, insertUrl,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response)
+                    {
+
+                    }
+                }, new Response.ErrorListener()
+        {
+
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError
+            {
+                Map<String, String> parameters = new HashMap<String, String>();
+                parameters.put("latitude", String.valueOf(Latitude));
+                parameters.put("longitude", String.valueOf(Longitude));
+                parameters.put("altitude", String.valueOf(Altitude));
+                parameters.put("model", String.valueOf(model));
+                parameters.put("date", String.valueOf(DateFormat.getDateTimeInstance().format(new Date())));
+
+                return parameters;
+            }
+        };
+        Log.i("the model is", model);
+
+        requestQueue.add(request);
     }
     public void onClickButtonShowDataFromServer()
     {
@@ -245,6 +281,38 @@ public class SendDataToServerActivity extends AppCompatActivity {
         });
 
     }
+    public void onClickButtonAutomaticallySendData()
+    {
+        toggleButtonAutomaticallySendData.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton cb, boolean on)
+            {
+
+                if (on)
+                {
+                    //Do something when Switch button is on/checked
+                    toggleButtonAutomaticallySendData.setText("STOP SENDING DATA !");
+                    toggleButtonAutomaticallySendData.setBackgroundColor(Color.RED);
+                    send=true;
+                    Thread t = new Thread(runnable);
+                    t.start();
+
+                } else
+                {
+                    //Do something when Switch is off/unchecked
+                    toggleButtonAutomaticallySendData.setText("AUTOMATICALLY SEND DATA");
+                    toggleButtonAutomaticallySendData.setBackgroundColor(Color.GREEN);
+                    send =false;
+                    t.interrupt();
+
+
+                }
+            }
+        });
+
+
+    }
+
 
 
 }
