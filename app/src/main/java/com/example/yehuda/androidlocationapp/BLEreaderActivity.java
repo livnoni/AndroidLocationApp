4f2fb3d6c4e4f749d7473c665f6d38ec608c9d87
livnoni <yehuda.livnoni@gmail.com>
2016-12-22 22:54:10 +0200

    Added BLE scanner.

--- app/src/main/java/com/example/yehuda/androidlocationapp/BLEreaderActivity.java
+++ app/src/main/java/com/example/yehuda/androidlocationapp/BLEreaderActivity.java
@@ -1,13 +1,56 @@
 package com.example.yehuda.androidlocationapp;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class BLEreaderActivity extends AppCompatActivity {
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothAdapter.LeScanCallback mLeScanCallback;
+    private boolean mScanning;
+    private Handler mHandler = new Handler();
+    // Stops scanning after 10 seconds.
+    private static final long SCAN_PERIOD = 10000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blereader);
+
+        // Initializes Bluetooth adapter.
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+        // displays a dialog requesting user permission to enable Bluetooth.
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
+        // Use this check to determine whether BLE is supported on the device. Then
+        // you can selectively disable BLE-related features.
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, "BLE not supported !", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        else
+        {
+            Toast.makeText(this, "cant find BLE singles!", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
+
 }
+
+
+
+
